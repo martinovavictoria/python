3f0917ec988eb5bc8abfe18db5d58d23f2ec5f8b
kartoshka2017 <martinovavictoria735@gmail.com>
2026-03-11 15:50:28 +0200

    Update 02_sum_and_dif.py

--- extra/hw1/02_sum_and_dif.py
+++ extra/hw1/02_sum_and_dif.py
@@ -7,15 +7,12 @@ def sum_of_digits(n):
 def count_digits(n):
     return len(str(n))
 
-# Ввод числа
 number = int(input("Введите число: "))
 
-# Вычисления
 sum_digits = sum_of_digits(number)
 count = count_digits(number)
 difference = sum_digits - count
 
-# Вывод результатов
 print(f"Сумма чисел: {sum_digits}")
 print(f"Количество цифр в числе: {count}")
 print(f"Разность суммы и количества цифр: {difference}")
