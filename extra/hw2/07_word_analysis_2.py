4df002ee53025a085fb580b59f14b88523b49799
kartoshka2017 <martinovavictoria735@gmail.com>
2026-03-11 15:59:05 +0200

    Create 07_word_analysis_2.py

---
+++ extra/hw2/07_word_analysis_2.py
@@ -0,0 +1,9 @@
+def is_palindrome(word):
+    return word == word[::-1]
+
+word = input("Введите слово: ")
+
+if is_palindrome(word):
+    print("Слово является палиндромом")
+else:
+    print("Слово не является палиндромом")
