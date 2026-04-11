1a3f55e58354d55343ded257926e402a0e6bf788
kartoshka2017 <martinovavictoria735@gmail.com>
2026-04-11 11:58:18 +0200

    Create 01_scary_code.py

---
+++ extra/hw3/01_scary_code.py
@@ -0,0 +1,17 @@
+# Основной список
+a = [1, 5, 3]
+b = [1, 5, 1, 5]
+c = [1, 3, 1, 5, 3, 3]
+
+a.extend(b)
+count_five = a.count(5)
+print(f"Кол-во цифр 5 при первом объединении: {count_five}")
+
+while 5 in a:
+    a.remove(5)
+  
+a.extend(c)
+count_three = a.count(3)
+print(f"Кол-во цифр 3 при втором объединении: {count_three}")
+
+print(f"Итоговый список: {a}")
