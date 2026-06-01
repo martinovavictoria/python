ac526f2acd65f7d23c1ab85eea1fdf774611f520
kartoshka2017 <martinovavictoria735@gmail.com>
2026-06-01 13:08:23 +0200

    Update ochered.py

--- ochered.py
+++ ochered.py
@@ -1,66 +1,32 @@
 class Node:
     def __init__(self, data):
-        self.data = data    # Хранит данные (например, имя покупателя)
-        self.next = None    # Ссылка на следующий элемент в списке
+        self.data = data
+        self.next = None
 
 
-class StoreQueue:
+class Stack:
     def __init__(self):
-        self.front = None # Кто первый на кассу
-        self.rear = None  # Кто последний в очереди
+        self.top = None
 
-    def enqueue(self, customer_name):
-        new_node = Node(customer_name)
-        if self.rear is None:
-            self.front = self.rear = new_node
-            return
-        self.rear.next = new_node
-        self.rear = new_node
-        print(f"{customer_name} встал в очередь.")
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+        print(f"{data} добавлен в стек")
 
-    def dequeue(self):
-        if self.front is None:
-            print("Очередь пуста")
+    def pop(self):
+        if self.top is None:
+            print("Стек пуст")
             return None
-        customer = self.front.data
-        self.front = self.front.next
-        if self.front is None:
-            self.rear = None
-        print(f"Покупатель {customer} обслужен.")
-        return customer
+
+        value = self.top.data
+        self.top = self.top.next
+        print(f"{value} удалён")
+        return value
 
     def peek(self):
-        return self.front.data if self.front else "Пусто"
-
-    class StoreQueue:
-        def __init__(self):
-            self.front = None  
-            self.rear = None  
-
-        def enqueue(self, customer_name):
-            new_node = Node(customer_name)
-            if self.rear is None:
-                self.front = self.rear = new_node
-                return
-            self.rear.next = new_node
-            self.rear = new_node
-            print(f"{customer_name} встал в очередь.")
-
-        def dequeue(self):
-            if self.front is None:
-                print("Очередь пуста!")
-                return None
-            customer = self.front.data
-            self.front = self.front.next
-            if self.front is None:
-                self.rear = None
-            print(f"Покупатель {customer} обслужен.")
-            return customer
-
-        def peek(self):
-            return self.front.data if self.front else "Пусто"
-
-
+        return self.top.data if self.top else "Пусто"
+        
 class StoreQueue:
     def __init__(self):
         self.front = None 
@@ -115,3 +81,14 @@ class Deque:
         if not self.head: self.tail = None
         return val
 
+stack = Stack()
+
+stack.push("Книга")
+stack.push("Тетрадь")
+stack.push("Ручка")
+
+print(stack.peek())
+
+stack.pop()
+
+print(stack.peek())
